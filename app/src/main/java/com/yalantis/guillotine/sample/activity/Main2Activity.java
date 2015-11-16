271642fe0c01e090c8a4cc7f6fd54e77d08a63ce
BillChabzz <chaby9.8@gmail.com>
2015-11-16 16:50:35 +0300

    new activity

--- app/src/main/java/com/yalantis/guillotine/sample/activity/Main2Activity.java
+++ app/src/main/java/com/yalantis/guillotine/sample/activity/Main2Activity.java
@@ -1,13 +1,12 @@
 package com.yalantis.guillotine.sample.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
-import android.widget.LinearLayout;
 
 import com.yalantis.guillotine.animation.GuillotineAnimation;
 import com.yalantis.guillotine.sample.R;
@@ -15,15 +14,7 @@ import com.yalantis.guillotine.sample.R;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
-/**
- * Created by Dmytro Denysenko on 5/4/15.
- */
-public class MainActivity extends AppCompatActivity {
-    LinearLayout prof;
-    LinearLayout feed;
-    LinearLayout activity;
-    LinearLayout settings;
-
+public class Main2Activity extends AppCompatActivity {
 
     private static final long RIPPLE_DURATION = 250;
 
@@ -38,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity);
+        setContentView(R.layout.profile);
         ButterKnife.inject(this);
 
 
@@ -56,24 +47,7 @@ public class MainActivity extends AppCompatActivity {
                 .setClosedOnStart(true)
                 .build();
 
-        prof = (LinearLayout)findViewById(R.id.profile_group);
-        feed = (LinearLayout)findViewById(R.id.feed_group);
-        activity = (LinearLayout)findViewById(R.id.activity_group);
-        settings = (LinearLayout)findViewById(R.id.settings_group);
-
-        prof.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switch(v.getId()){
-                    case R.id.profile_group:
-                        Intent b = new Intent(MainActivity.this,Main2Activity.class);
-                        startActivity(b);
-                     break;
-
-                }
-            }
-        });
-
 
     }
+
 }
